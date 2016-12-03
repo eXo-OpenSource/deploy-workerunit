90231b82f100ef8c17a37cb149dfd7953b585346
Jusonex <jusonex96@gmail.com>
2016-12-03 19:54:48 +0100

    Add some log outputs and fix editorconfig

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	// Create MTA server instance
+	fmt.Println("Creating MTAServer...")
 	server := NewMTAServer("/var/lib/mtasa/mta-server64")
 
 	// Create api
+	fmt.Println("Creating API...")
 	api := NewApi(server)
 
 	// Listen for requests on the main goroutine
+	fmt.Println("Waiting for commands...")
 	api.Listen()
 }
